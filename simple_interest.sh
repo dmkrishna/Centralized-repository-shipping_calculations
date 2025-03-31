ed30ca04c0500c469bb9ca8e460ee4582434bfb2
dmkrishna <dmkrishna@yahoo.com>
2025-03-30 23:57:50 -0400

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,10 @@
+
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
+
+# Do not use this in production. Sample purpose only.
+
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
